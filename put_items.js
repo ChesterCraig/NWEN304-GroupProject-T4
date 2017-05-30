4816d113b77fef3678f889c658c92798cad57a57
reubenls <reubenls@outlook.com>
2017-05-30 22:05:29 +1200

    added some clothing images and js to put items in database

---
+++ put_items.js
@@ -0,0 +1,109 @@
+const {client} = require("./Database/pg");
+const query = require("./Database/queries");
+var items = [
+{
+	"item": {
+	"name":"Red Casual Shirt",
+	"description":"Red Short Sleeve Shirt -Casual",
+	"price":22.50,
+	"image_path":"/images/cshirt1.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Blue Casual Shirt",
+	"description":"Blue Long Sleeve Shirt",
+	"price":28.50,
+	"image_path":"/images/cshirt2.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Black Dress",
+	"description":"Black Sleeveless Dress",
+	"price":39.99,
+	"image_path":"/images/dress1.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Pink Dress",
+	"description":"Pink Sleeveless Dress",
+	"price":35.99,
+	"image_path":"/images/dress2.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Dark Blue Jeans",
+	"description":"Dark Blue Jeans - Faded",
+	"price":45.00,
+	"image_path":"/images/jeans1.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Light Blue Jeans",
+	"description":"Light Blue Jeans - Loose fit",
+	"price":45.00,
+	"image_path":"/images/jeans2.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Brown Shoes",
+	"description":"Brown Leather Shoes",
+	"price":65.00,
+	"image_path":"/images/shoe1.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Black Shoes",
+	"description":"Black Leather Shoes",
+	"price":65.00,
+	"image_path":"/images/shoe1.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Denim Shorts",
+	"description":"Black Denim Shorts",
+	"price":25.00,
+	"image_path":"/images/shorts1.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Checkered Shorts",
+	"description":"Blue Checkered Denim Shorts",
+	"price":25.00,
+	"image_path":"/images/shorts2.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Blue T-Shirt",
+	"description":"Blue T-Shirt - Writing",
+	"price":15.00,
+	"image_path":"/images/tshirt2.jpg"
+	}
+},
+{
+	"item": {
+	"name":"Yellow Shorts",
+	"description":"Yellow T-Shirt - Writing",
+	"price":15.00,
+	"image_path":"/images/tshirt.jpg"
+	}
+}
+];
+
+items.forEach(function(item){
+    query.createItem(client,item.item,function(error,results){
+        if (error) {
+            console.log(error);
+        }
+    })
+});
+
