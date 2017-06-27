4bf06267faa7825c8a33dbe06d2f8f722fec03c2
ChesterCraig <chester.craig@gmail.com>
2017-06-27 20:26:54 +1200

    made item search case-insensitive

--- Database/queries.js
+++ Database/queries.js
@@ -30,7 +30,7 @@ module.exports = q;
 //========== ITEMS ========================
 
 q.getItems = function(client,callback) {
-    var query = client.query('select id, name, gender, description, price, image_path from ITEM');
+    var query = client.query('select id, name, gender, description, price, image_path from ITEM;');
     var results = [];
 
     // Handle error
@@ -50,7 +50,7 @@ q.getItems = function(client,callback) {
 };
 
 q.getItem = function(client,id,callback) {
-    var query = client.query(`select id, name, gender, description, price, image_path from ITEM where id = ${id}`);
+    var query = client.query(`select id, name, gender, description, price, image_path from ITEM where id = ${id};`);
     results = [];
 
     // Handle error
@@ -71,7 +71,7 @@ q.getItem = function(client,id,callback) {
 
 q.createItem = function(client, details, callback) {
     var queryString = `INSERT INTO item (name, gender, description, price, image_path) VALUES ('${details.name}', '${details.gender}','${details.description}',${details.price},'${details.image_path}') `;
-    queryString = queryString + `RETURNING id, name, gender, description, price, image_path`;
+    queryString = queryString + `RETURNING id, name, gender, description, price, image_path;`;
     var query = client.query(queryString);
     var results = [];
 
@@ -92,7 +92,7 @@ q.createItem = function(client, details, callback) {
 };
 
 q.getMaleItems = function (client, callback) {
-    var queryString = `select * from ITEM where gender = 'Male'`;
+    var queryString = `select * from ITEM where gender = 'Male';`;
     var query = client.query(queryString);
     var results = [];
 
@@ -113,7 +113,7 @@ q.getMaleItems = function (client, callback) {
 };
 
 q.getFemaleItems = function (client, callback) {
-    var queryString = `select * from ITEM where gender = 'Female'`;
+    var queryString = `select * from ITEM where gender = 'Female';`;
     var query = client.query(queryString);
     var results = [];
 
@@ -136,7 +136,7 @@ q.getFemaleItems = function (client, callback) {
 q.itemSearch = function (client, name, callback) {
     console.log(name);
     var name = name;
-    var queryString = `select * from item where name like '%`+name+`%';`;
+    var queryString = `select * from item where lower(name) like lower('%`+name+`%');`;
     var query = client.query(queryString);
     var results = [];
 
@@ -167,7 +167,7 @@ q.deleteItem = function(client, id, callback) {
 
 // Get Users
 q.getUsers = function (client, callback) {
-    var query = client.query(`select id, email, facebook_id, display_name, is_admin from user_account`);
+    var query = client.query(`select id, email, facebook_id, display_name, is_admin from user_account;`);
     var results = [];
 
     // Handle error
@@ -189,11 +189,11 @@ q.getUsers = function (client, callback) {
 // Get User (supports id, facebook_id or email)
 q.getUser = function (client,details,callback) {
     if (details.id) {
-        var query = client.query(`SELECT id, email, facebook_id, display_name, is_admin FROM USER_ACCOUNT WHERE id = ${details.id}`);
+        var query = client.query(`SELECT id, email, facebook_id, display_name, is_admin FROM USER_ACCOUNT WHERE id = ${details.id};`);
     } else if (details.facebook_id) {
-        var query = client.query(`SELECT id, email, facebook_id, display_name, is_admin FROM USER_ACCOUNT WHERE facebook_id = '${details.facebook_id}'`);
+        var query = client.query(`SELECT id, email, facebook_id, display_name, is_admin FROM USER_ACCOUNT WHERE facebook_id = '${details.facebook_id}';`);
     } else if (details.email) {
-        var query = client.query(`SELECT id, email, facebook_id, display_name, is_admin FROM USER_ACCOUNT WHERE email = '${details.email}'`);
+        var query = client.query(`SELECT id, email, facebook_id, display_name, is_admin FROM USER_ACCOUNT WHERE email = '${details.email}';`);
     } else {
         return callback("Invalid, no ID provided");
     }
@@ -218,11 +218,11 @@ q.getUser = function (client,details,callback) {
 
 q.getUserPasswordHash = function (client,details,callback) {
     if (details.id) {
-        var query = client.query(`SELECT id, password_hash FROM USER_ACCOUNT WHERE id = ${details.id}`);
+        var query = client.query(`SELECT id, password_hash FROM USER_ACCOUNT WHERE id = ${details.id};`);
     } else if (details.facebook_id) {
-        var query = client.query(`SELECT id, password_hash FROM USER_ACCOUNT WHERE facebook_id = '${details.facebook_id}'`);
+        var query = client.query(`SELECT id, password_hash FROM USER_ACCOUNT WHERE facebook_id = '${details.facebook_id}';`);
     } else if (details.email) {
-        var query = client.query(`SELECT id, password_hash FROM USER_ACCOUNT WHERE email = '${details.email}'`);
+        var query = client.query(`SELECT id, password_hash FROM USER_ACCOUNT WHERE email = '${details.email}';`);
     } else {
         return callback("Invalid, no ID provided");
     }
@@ -247,7 +247,7 @@ q.getUserPasswordHash = function (client,details,callback) {
 
 // Create User
 q.createFaceBookUser = function (client,details,callback) {
-    var query = client.query(`INSERT INTO USER_ACCOUNT (facebook_id, display_name) VALUES ('${details.facebook_id}','${details.displayName}') RETURNING id, facebook_id, display_name`);
+    var query = client.query(`INSERT INTO USER_ACCOUNT (facebook_id, display_name) VALUES ('${details.facebook_id}','${details.displayName}') RETURNING id, facebook_id, display_name;`);
     var results = [];
 
     // Handle error
@@ -270,10 +270,10 @@ q.createLocalUser = function (client,details,callback) {
     if (details.isAdmin == true) {
         var queryString = `INSERT INTO USER_ACCOUNT (email,password_hash,display_name,is_admin) `;
         queryString = queryString + `VALUES ('${details.email}','${details.password_hash}','${details.displayName}','TRUE') `;
-        queryString = queryString + `RETURNING id, email, display_name`;
+        queryString = queryString + `RETURNING id, email, display_name;`;
     } else {
         var queryString = `INSERT INTO USER_ACCOUNT (email,password_hash,display_name) VALUES ('${details.email}','${details.password_hash}','${details.displayName}') `;
-        queryString = queryString + `RETURNING id, email, display_name`;
+        queryString = queryString + `RETURNING id, email, display_name;`;
     }
 
     var query = client.query(queryString);
@@ -308,7 +308,7 @@ q.deleteUser = function (client,id,callback) {
 
 q.createBasketItem = function (client,details,callback) {
     var queryString = `INSERT INTO basket_item (user_account, item, quantity) VALUES (${details.user_account_id},${details.item},${details.quantity}) `;
-    queryString = queryString + `RETURNING id, item, quantity, user_account`;
+    queryString = queryString + `RETURNING id, item, quantity, user_account;`;
     
     var query = client.query(queryString);
     var results = [];
@@ -331,24 +331,24 @@ q.createBasketItem = function (client,details,callback) {
 
 q.deleteBasketItem = function (client,details,callback) {
     if (details.user_account_id) {
-        client.query(`DELETE FROM BASKET_ITEM WHERE id = ${details.id} and user_account = ${details.user_account_id}`, function(error) {
+        client.query(`DELETE FROM BASKET_ITEM WHERE id = ${details.id} and user_account = ${details.user_account_id};`, function(error) {
             callback(error);
         });
     } else {
-        client.query(`DELETE FROM BASKET_ITEM WHERE id = ${details.item}`, function(error) {
+        client.query(`DELETE FROM BASKET_ITEM WHERE id = ${details.item};`, function(error) {
             callback(error);
         });
     }
 };
 
 q.deleteBasketItems = function (client,id,callback) {
-    client.query(`DELETE FROM BASKET_ITEM WHERE user_account = ${id}`, function(error) {
+    client.query(`DELETE FROM BASKET_ITEM WHERE user_account = ${id};`, function(error) {
         callback(error);
     });
 };
 
 q.getAllBasketItems = function (client, id, callback) {
-    var queryString = `select id, item, quantity from BASKET_ITEM`;
+    var queryString = `select id, item, quantity from BASKET_ITEM;`;
     var query = client.query(queryString);
     var results = [];
 
@@ -369,7 +369,7 @@ q.getAllBasketItems = function (client, id, callback) {
 };
 
 q.getBasketItems = function (client, id, callback) {
-    var queryString = `select id, item, quantity from BASKET_ITEM where user_account = ${id}`;
+    var queryString = `select id, item, quantity from BASKET_ITEM where user_account = ${id};`;
     var query = client.query(queryString);
     var results = [];
 
@@ -391,7 +391,7 @@ q.getBasketItems = function (client, id, callback) {
 
 q.updateBasketItem = function (client,details,callback) {
     var queryString = `UPDATE basket_item set quantity = ${details.quantity} where id = ${details.item} and user_account = ${details.user_account_id} `;
-    queryString = queryString + `RETURNING id, item, quantity, user_account`;
+    queryString = queryString + `RETURNING id, item, quantity, user_account;`;
     
     var query = client.query(queryString);
     var results = [];
