88e6b8ffe7d195759477c26688839fcc3890a766
reubenls <reubenls@outlook.com>
2017-07-05 11:13:06 +1200

    https?

--- index.js
+++ index.js
@@ -17,6 +17,15 @@ const {client} = require("./Database/pg");
 // Create express app
 var app = express();
 
+
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https'){
+      res.redirect(`https://${req.header('host')}${req.url}`);}
+    else {
+      next();}
+  });
+
+
 // Setup pug/jade view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
